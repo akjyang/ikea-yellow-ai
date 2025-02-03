2a4a88b1d4ddb68424ef8239a293b07595310e3d
Andrew Yang <akjyang@wharton.upenn.edu>
2025-02-03 02:44:48 +0000

    Moved master folder

--- waf-ikea-master/scripts/run.sh
+++
@@ -1,4 +0,0 @@
-#!/usr/bin/env bash
-
-export PYTHON_PREFIX=~/miniforge
-${PYTHON_PREFIX}/bin/streamlit run ../run.py --server.enableCORS=false --server.enableXsrfProtection=false --server.port 8080
